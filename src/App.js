194d54bce728b653afbe18d817e20af1113499cf
maryam-ajmairi <mariumajmairi@gmail.com>
2018-10-21 10:15:34 +0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,98 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import * as firebase from 'firebase';
 import './App.css';
+import './config/config';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+    constructor() {
+        super();
+        this.state = {
+            Task: []
+        };
+        this.ref = firebase.database().ref();
+    }
+
+        handleChange = event => {
+            this.setState({ [event.target.id]: event.target.value });
+        };
+
+        saveData = () => {
+             this.ref.child('tasks').push(this.state)
+        };
+
+
+            getData = () => {
+                this.ref.child('tasks').on('value', snapshot => {
+                    const obj = snapshot.val();
+                    const data = [];
+                    for (let key in obj) {
+                        data.push(obj[key]);
+                    }
+                    this.setState({ Task : data });
+                });
+            };
+
+
+    render() {
+        const { classes } = this.props;
+        return (
+          <div>
+          <Card className={classes.card}>
+          <CardContent>
+            <TextField
+              id="Tasks"
+              label="Task"
+              fullWidth
+              onChange={this.handleChange}
+            />
+            <br />         
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="extendedFab"
+              fullWidth
+              color="primary"
+              onClick={this.saveData}
+            >
+              Add Task
+            </Button>
+          </CardActions>
+        </Card>
+        <ul>
+          {this.state.Task.map((value, index) => {
+              return (
+                <li key={index}>
+                {value.Task}
+              </li>
+            );
+        })}
+        </ul>
       </div>
+
+     
     );
   }
 }
+App.propTypes = {
+    classes: PropTypes.object.isRequired
+};
 
-export default App;
+const styles = {
+    card: {
+        minWidth: 300,
+        maxWidth: 500
+    },
+    container: {
+        display: 'flex',
+        justifyContent: 'center'
+    }
+};
+export default withStyles(styles)(App);
---
+++ src/config/config.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+
+var config = {
+    apiKey: "AIzaSyCO3wQqh1Azf1XsXfuwezSu6i7bRYEV2-Q",
+    authDomain: "todoapp-react-firebase.firebaseapp.com",
+    databaseURL: "https://todoapp-react-firebase.firebaseio.com",
+    projectId: "todoapp-react-firebase",
+    storageBucket: "todoapp-react-firebase.appspot.com",
+    messagingSenderId: "563673643733"
+};
+firebase.initializeApp(config);
